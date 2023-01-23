03751e7c13e7eade14ea0249c744ec35a202ed2f
Ged Kitchener <ged.kitchener@charlottetilbury.com>
2023-01-23 21:50:05 +0000

    Initial commit - overarching steps

---
+++ aerial_photo_locator.py
@@ -0,0 +1,11 @@
+# TODO: train model to detect roads and buildings from an aerial photo.
+#  See Saito et al 2015 - https://www.researchgate.net/publication/273453681_Building_and_road_detection_from_large_aerial_imagery
+#  and https://www.todaysoftmag.com/article/2428/detecting-roads-from-aerial-images-using-deep-learning.
+
+# TODO: use model to detect roads and buildings in unseen aerial photo
+
+# TODO: re-position photo as if it were birds-eye-view
+
+# TODO: find matching section on known map. See https://stackoverflow.com/a/10169025/2660406
+#  and https://docs.opencv.org/2.4/doc/tutorials/features2d/feature_homography/feature_homography.html#feature-homography
+#  and OpenStreetMap - https://towardsdatascience.com/loading-data-from-openstreetmap-with-python-and-the-overpass-api-513882a27fd0
